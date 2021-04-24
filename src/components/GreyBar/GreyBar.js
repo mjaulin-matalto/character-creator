dfa992a263fb575c4c9c0a93d09c0d1905615c72
Mathieu JAULIN <mathieu.jaulin@yahoo.fr>
2021-04-24 22:16:34 +0200

    Adding a 40%-screen-size light Grey bar to add perspective

---
+++ src/components/GreyBar/GreyBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './GreyBar.module.css';
+
+const GreyBar = () => {
+    return (
+        <div className={styles.greybar} />
+    );
+};
+
+export default GreyBar;
