bf58d5bacf864531cede073e6dafba71e07cbf05
Mathieu JAULIN <mathieu.jaulin@yahoo.fr>
2021-04-24 23:21:27 +0200

    Align with some of Josh explanation - less component, more lean effect, more analysis needed in general

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
-import GreyBar from "./components/GreyBar";
-import EditorWrapper from "./components/EditorWrapper"
 
 function App() {
   return (
     <>
-      <GreyBar />
-      <EditorWrapper>
-          <CharacterEditor />
-          <Footer />
-      </EditorWrapper>
+      <CharacterEditor />
+      <Footer />
     </>
   );
 }
